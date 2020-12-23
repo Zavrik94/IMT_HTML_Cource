3126e5852db095d20a63fa78ebab5701b161398f
Zavrik94 <Arty.zavrik@gmail.com>
2020-12-23 20:54:51 +0200

    Module13

---
+++ Module13/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Module13</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> -->
+	<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script> -->
+</head>
+<body>
+<div class="container">
+		<header>
+			<div class="logo">
+				<img src="logo2.png">
+			</div>
+			<nav>
+					<ul>
+						<li><a href="#">Main</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contacts</a></li>
+					</ul>
+			</nav>
+		</header>
+		<div>
+			<div>
+				left
+			</div>
+			<div>
+				content
+			</div>
+			<div>
+				info
+			</div>
+		</div>
+		<footer>
+			footer
+		</footer>
+	</div>
+</body>
+<!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script> -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script> -->
+</html>
